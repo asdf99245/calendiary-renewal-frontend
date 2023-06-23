f3078e698b7032a4d58407e7c295612f6c2168f2
Yunkyu Jung <39851220+asdf99245@users.noreply.github.com>
2023-06-23 18:11:35 +0900

    🎨 design: tailwind custom color token 정의
    - semantic color naming으로 darkmode 색상 구분

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,44 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        primary_variant: 'rgb(var(--color-primary-variant) / <alpha-value>)',
+        primary_gradient: 'linear-gradient(to bottom, #9969BF, #BF69B9)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        secondary_variant: 'rgb(var(--color-secondary-variant) / <alpha-value>)',
+        error: 'rgb(var(--color-error) / <alpha-value>)',
+        kakao_yellow: 'FEE500',
+        naver_green: '03C75A',
+        bg: {
+          primary: 'rgb(var(--color-bg-primary) / <alpha-value>)',
+          secondary: 'rgb(var(--color-bg-secondary) / <alpha-value>)',
+          tertiary: 'rgb(var(--color-bg-tertiary) / <alpha-value>)',
+        },
+        text: {
+          primary: 'rgb(var(--color-text-primary) / <alpha-value>)',
+          secondary: 'rgb(var(--color-text-secondary) / <alpha-value>)',
+          tertiary: 'rgb(var(--color-text-tertiary) / <alpha-value>)',
+        },
+        border: {
+          primary: 'rgb(var(--color-border-primary) / <alpha-value>)',
+          secondary: 'rgb(var(--color-border-secondary) / <alpha-value>)',
+        },
+        palette: {
+          white: '#FFFFFF',
+          black: '#000000',
+          yellow: '#DC9B18',
+          blue: '#186ADC',
+          green: '#18DC74',
+          red: '#EA553E',
+        },
       },
     },
   },
