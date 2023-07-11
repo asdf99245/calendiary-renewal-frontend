3d297780d1b36464223d6ed444bec5c6566c861d
asdf99245 <asdf99245@naver.com>
2023-07-11 23:33:26 +0900

    ✨ feat: icon 컴포넌트 size, color default 값 적용

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,5 +1,6 @@
 import { SVGProps } from 'react';
 import * as icons from '@/assets/icons';
+import { cn } from '@/utils/cn';
 
 interface Props extends SVGProps<SVGSVGElement> {
   iconType: keyof typeof icons;
@@ -7,8 +8,16 @@ interface Props extends SVGProps<SVGSVGElement> {
   size?: number | string;
 }
 
-export function Icon({ iconType, color, size, ...props }: Props) {
+export function Icon({ iconType, color, size = 24, className, ...props }: Props) {
   const SVGIcon = icons[iconType];
 
-  return <SVGIcon fill={color} width={size} height={size} {...props} />;
+  return (
+    <SVGIcon
+      fill={color}
+      width={size}
+      height={size}
+      className={cn({ 'fill-text-primary': !color }, className)}
+      {...props}
+    />
+  );
 }
