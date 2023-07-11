f9224c6f1f93851b16e1c08d4aad30584dd6f524
asdf99245 <asdf99245@naver.com>
2023-07-11 23:35:22 +0900

    🐛 fix: sample 컴포넌트에서 Icon 제거

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -1,10 +1,7 @@
-import Add from '@/assets/icons/add.svg';
-
 export default function Sample() {
   return (
     <span>
-      This Is Sample
-      <Add width={24} height={24} />
+      <span>This Is Sample</span>
     </span>
   );
 }
